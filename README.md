01236d5dd9bfcbfc98ff8b6b298a6d52f825aa5f
Miguel Boanova <Miguel Boanova>
2016-11-01 17:20:37 -0200

    Modificado background com clip-path e modificado navbar pós scroll

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# mglnb.github.io" 
+<h1>Primeiro projeto com GitHub</h1>
+
+<h3>"Se você empregasse seu tempo em estudar, pensar e planejar todos os dias poderia desenvolver e usar o poder que talvez mudasse todo o curso do seu destino."</h3>
---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function($) {
+	$('header').addClass('animacao');
+	console.log('add')	
+
+
+	var num = 50; // tanto de scroll que vai precisar para a barra ficar fixa.
+
+	$(window).bind('scroll', function () {
+		if ($(window).scrollTop() > num) {
+			$('.navbar-fixed-top').addClass('navbar-scroll');
+		} else {
+	       //Quando o menu ficar fixo
+	       $('.navbar-fixed-top').removeClass('navbar-scroll'); 
+	   }
+	});
+});
