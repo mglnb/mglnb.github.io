177c2839df5f17d7fd249adfc93d78aa45389b0e
Miguel Boanova <Miguel Boanova>
2016-11-03 14:50:45 -0200

    Iniciando portfólio com JQuery para filtro

--- js/script.js
+++ js/script.js
@@ -2,14 +2,34 @@ $(document).ready(function($) {
 	$('header').addClass('animacao');
 
 
-	var num = 50; // tanto de scroll que vai precisar para a barra ficar fixa.
 
 	$(window).bind('scroll', function () {
-		if ($(window).scrollTop() > num) {
+		if ($(window).scrollTop() > 50) {
 			$('.navbar-fixed-top').addClass('navbar-scroll');
 		} else {
 	       //Quando o menu ficar fixo
 	       $('.navbar-fixed-top').removeClass('navbar-scroll'); 
 	   }
 	});
+
+	$('#portfolio .content ul li a').click(function(event) {
+		if ($(this).is('#todos')) {
+			$('.c1').show('slow');
+			$('.c2').show('slow');
+			$('.c3').show('slow');
+		} else if ($(this).is('#fotos')) {
+			$('.c1').hide('slow');
+			$('.c2').show('slow');
+			$('.c3').hide('slow');
+		} else if ($(this).is('#sites')) {
+			$('.c1').show('slow');
+			$('.c2').hide('slow');
+			$('.c3').hide('slow');
+		} else if ($(this).is('#logos')) {
+			$('.c1').hide('slow');
+			$('.c2').hide('slow');
+			$('.c3').show('slow');
+		}
+	});
+
 });
