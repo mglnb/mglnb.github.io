16a2a18d8cffb71a67a8f95fc000584b8d114a52
Miguel Boanova <mgl.deadly@gmail.com>
2019-01-25 00:01:30 +0000

    Updates

---
+++ precache-manifest.739e53d1ad6e71f795b2331c12fb30a4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42c4955d85d3bba91261",
+    "url": "/static/js/main.42c4955d.chunk.js"
+  },
+  {
+    "revision": "c9cd70f78e6133b4fa45",
+    "url": "/static/js/1.c9cd70f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "764ad3c0e8761a5d0d981c8af9e8913a",
+    "url": "/index.html"
+  }
+];
